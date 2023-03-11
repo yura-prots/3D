1c2804d52c012c0be9ad9614e658e4214859d504
yura-prots <yuraprots@gmail.com>
2023-03-11 20:53:09 +0200

    add ComputersCanvas, Loader

--- app/src/components/canvas/Computers.jsx
+++ app/src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -29,4 +29,28 @@ const Computers = () => {
   );
 };
 
-export default Computers;
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
