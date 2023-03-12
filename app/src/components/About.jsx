264827d86180c9638fcea5e3816f1699b8eae42d
yura-prots <yuraprots@gmail.com>
2023-03-12 12:06:12 +0200

    About completed

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -8,11 +8,14 @@ import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ title, icon, index }) => {
+  const nodeRef = React.useRef(null);
+
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        nodeRef={nodeRef}
       >
         <div
           options={{
